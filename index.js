52c0025c51b2bf33deb2e6e4d56624c98215ac52
Randy Burgess <wrburgess@gmail.com>
2017-12-12 18:22:36 -0600

    saving work in progress

--- index.js
+++ index.js
@@ -7,32 +7,37 @@ io.on('connection', function(socket){
   console.log('a user is connected');
 
   socket.on('next', function(msg){
-    console.log('broadcasting "next" message');
+    console.log('broadcasting "next" message: ' + msg);
     io.emit('next', msg);
   });
 
   socket.on('previous', function(msg){
-    console.log('broadcasting "previous" message');
+    console.log('broadcasting "previous" message: ' + msg);
     io.emit('previous', msg);
   });
 
   socket.on('redirect', function(msg){
-    console.log('broadcasting "redirect" message');
+    console.log('broadcasting "redirect" message: ' + msg);
     io.emit('redirect', msg);
   });
 
   socket.on('statusUpdate', function(msg){
-    console.log('broadcasting "status" message');
+    console.log('broadcasting "status" message: ' + msg);
     io.emit('statusUpdate', msg);
   });
+
   socket.on('blackout', function(msg){
+    console.log('broadcasting "blackout" message: ' + msg);
     io.emit('blackout', msg);
   });
+
   socket.on('fullscreen', function(msg){
+    console.log('broadcasting "fullscreen" message: ' + msg);
     io.emit('fullscreen', msg);
   });
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function(msg){
+    console.log('broadcasting "disconnect" message: ' + msg);
     console.log('a user has disconnected');
   });
 });
@@ -50,8 +55,6 @@ app.get('/client', function(req, res){
 });
 
 app.use(express.static(__dirname + '/public'));
-
-
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
